20f00d89aba80db1562451dfb22733b18aabb136
Prathiksha9876 <prathiksha9876@gmail.com>
2026-04-06 21:33:42 +0530

    Change API base URL to production endpoint

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://product-feedback-app-1-fgie.onrender.com',
 });
 
 api.interceptors.request.use(
