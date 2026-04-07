7ac8e4c58872bf301ccc16b47a3fa44c7cbf1a79
Prathiksha9876 <prathiksha9876@gmail.com>
2026-04-07 11:08:54 +0530

    Change axios base URL to include '/api'
    Updated the base URL in axios configuration to include '/api'.

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,27 @@
+// import axios from 'axios';
+
+// const api = axios.create({
+//   baseURL: 'https://product-feedback-app-1-fgie.onrender.com',
+// });
+
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem('token');
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default api;
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://product-feedback-app-1-fgie.onrender.com',
+  baseURL: 'https://product-feedback-app-1-fgie.onrender.com/api',
 });
 
 api.interceptors.request.use(
